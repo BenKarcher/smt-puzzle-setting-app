49c70ae2c17002037f47b384f7f9aff08bd5c3b9
Ben Karcher <bnkarcher@gmail.com>
2025-10-23 01:58:22 -0700

    updated conditional base for dev

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/smt-puzzle-setting-app/',
+  base: mode === 'production' ? '/smt-puzzle-setting-app/' : '/',
   optimizeDeps: {
     exclude: ['minizinc']
   },
